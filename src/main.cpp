212ccb50a642ec2690afa3f7472a53d796547aba
VoTanDat <dat.tandat1247@gmail.com>
2022-05-09 23:05:44 +0700

    Add BLE code

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@
 
 #include "MAX30105.h"
 #include "heartRate.h"
-//#include "spo2_algorithm.h"
 
-#define USEFIFO
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define bleServerName "FPT_HC_IoTS_BTempHRate01-1E9E"
+#define SERVICE_UUID "7bde7b9d-547e-4703-9785-ceedeeb2863e"
+#define CHARACTERISTIC_UUID "9d45a73a-b19f-4739-8339-ecad527b4455"
 
 #define SCREEN_WIDTH 128    // OLED display width, in pixels
 #define SCREEN_HEIGHT 64    // OLED display height, in pixels
@@ -21,6 +27,24 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 MAX30105 particleSensor;
 
+/* BLE Variable */
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+uint8_t value[] = "HR:xxx,SPO2:xx,TEMP:xx";
+
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
 /* Temp varialble */
 double temp_obj;
 
@@ -51,12 +75,29 @@ double ESpO2 = 95.0;    //initial value of estimated SpO2
 double FSpO2 = 0.7;
 double frate = 0.95;    //low pass filter for IR/red LED value to eliminate AC component
 
+void BLE_Setup(void);
 void OLED_Display(double temp, int heartBeat, int32_t SPO2);
 
 void task_Temp(void *parameter) {
   for(;;) {
     temp_obj = mlx.readObjectTempC();
     OLED_Display(temp_obj, beatAvg, oxygen);
+
+    if (deviceConnected) 
+    {
+      value[3] = beatAvg/100 + 48;
+      value[4] = (beatAvg - ((value[3]-48)*100))/10 + 48;
+      value[5] = beatAvg%10 + 48;
+
+      value[12] = oxygen/10 + 48;
+      value[13] = oxygen%10 + 48;
+
+      value[20] = (int)temp_obj/10 + 48;
+      value[21] = (int)temp_obj%10 + 48;
+
+      pCharacteristic->setValue((uint8_t*)&value, sizeof(value));
+      pCharacteristic->notify();
+    }
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
@@ -169,6 +210,8 @@ void setup() {
   display.setTextSize(1);
   display.print("Initializing");
   display.display();
+
+  BLE_Setup();
   delay(2000);
   xTaskCreate(task_Temp,"Task 1",8192,NULL,2,NULL); // (hàm thực thi, tên đặt cho hàm, stack size, context đưa vào argument của task, độ ưu tiên của task, reference để điều khiển task)
   xTaskCreate(task_MAX3010x,"Task 2",8192,NULL,1,NULL);
@@ -179,6 +222,33 @@ void loop() {
   
 }
 
+void BLE_Setup(void)
+{
+  BLEDevice::init(bleServerName);
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  // Create a BLE Characteristic
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+}
 void OLED_Display(double temp, int heartBeat, int32_t SPO2)
 {
   display.clearDisplay();
