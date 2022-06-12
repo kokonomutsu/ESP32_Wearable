1ab7ddd47077c15c881a0bf31ed1674c084f5a6d
VoTanDat <dat.tandat1247@gmail.com>
2022-06-12 11:07:37 +0700

    add .pio file

---
+++ .pio/build/esp32dev/libd38/BLE_Lib/Queue.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/libd38/BLE_Lib/Queue.cpp.o: lib/BLE_Lib/Queue.cpp \
+ lib/BLE_Lib/Queue.h
---
+++ .pio/build/esp32dev/libdb0/SPI/SPI.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/libdb0/SPI/SPI.cpp.o: \
+ C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
+ C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
