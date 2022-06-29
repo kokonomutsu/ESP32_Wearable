44a807583da23a4ac7ae3cd2d0c7451edc943373
MrKokono <sonhai26988@gmail.com>
2022-06-29 08:58:29 +0700

    Stop tracking built file

--- .pio/build/esp32dev/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USB.cpp.o: \
- C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/DThree/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/config/sdkconfig.h
